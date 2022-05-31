0a0fed3839e8689e1203066c33197dec81bfe6af
Matheus Rissi <matheusrissi5@gmail.com>
2022-05-31 12:54:12 -0300

    Subindo primeiro projeto

---
+++ urna_eletronica/etapas.js
@@ -0,0 +1,25 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 10,
+                nome: "Bolsonaro",
+                partido: 'ABC',
+                fotos: [
+                    {url:'jair-bolsonaro.jpeg', legenda: 'Presidente'}
+                ]
+            },
+
+            {
+                numero: 20,
+                nome: 'Lula',
+                partido: 'PT',
+                fotos: [
+                    {url:'lula-e1632603781683-1078x720.jpg', legenda: 'Presidente'}
+              ,  ]
+            },
+        ]
+    }
+]
